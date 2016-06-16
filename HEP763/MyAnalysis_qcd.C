971c6427c009a540462738b485dc0308a699867b
Dooyeon Gyun <dygyun@Dooyeonui-MacBook-Air.local>
2016-06-16 16:40:10 +0900

    QCD

---
+++ HEP763/MyAnalysis_qcd.C
@@ -0,0 +1,252 @@
+#define MyAnalysis_cxx
+#include "MyAnalysis.h"
+#include <iostream>
+#include <TH1F.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TH2F.h>
+#include <TLatex.h>
+using namespace std;
+void MyAnalysis::BuildEvent() {
+   
+   Muons.clear();
+   for (int i = 0; i < NMuon; ++i) {
+      MyMuon muon(Muon_Pt[i], Muon_Eta[i], Muon_Phi[i], Muon_E[i]);
+      muon.SetIsolation(Muon_Iso[i]);
+      muon.SetCharge(Muon_Charge[i]);
+      Muons.push_back(muon);
+   }
+   
+   Electrons.clear();
+   for (int i = 0; i < NElectron; ++i) {
+      MyElectron electron(Electron_Pt[i], Electron_Eta[i], Electron_Phi[i], Electron_E[i]);
+      electron.SetIsolation(Electron_Iso[i]);
+      electron.SetCharge(Electron_Charge[i]);
+      Electrons.push_back(electron);
+   }
+   
+   Photons.clear();
+   for (int i = 0; i < NPhoton; ++i) {
+      MyPhoton photon(Photon_Pt[i], Photon_Eta[i], Photon_Phi[i], Photon_E[i]);
+      photon.SetIsolation(Photon_Iso[i]);
+      Photons.push_back(photon);
+   }
+   
+   Jets.clear();
+   for (int i = 0; i < NJet; ++i) {
+      MyJet jet(Jet_Pt[i], Jet_Eta[i], Jet_Phi[i], Jet_E[i]);
+      jet.SetBTagDiscriminator(Jet_bDiscriminator[i]);
+      //jet.SetJetID(Jet_ID[i]);
+      Jets.push_back(jet);
+   }
+   mymet.SetXYZM(MET_Px, MET_Py, 0., 0.);
+   
+   EventWeight = PUWeight*GenWeight*weight_factor*norm_scale;
+}
+
+void MyAnalysis::Begin(TTree * /*tree*/) {
+   // The tree argument is deprecated (on PROOF 0 is passed).
+   TString option = GetOption();
+}
+
+void MyAnalysis::SlaveBegin(TTree * /*tree*/) {
+   // The SlaveBegin() function is called after the Begin() function.
+   TString option = GetOption();
+
+   for(int i=0; i < 4; i++){ 
+     hIso_vs_MET[i] = new TH2D(Form("hIso_vs_MET%i",i), "PF Isolation vs {#slash{E}_{T}}", 50 , 0, 200 , 100, 0 , 2.0 );
+     hIso_vs_MET[i]->SetXTitle("#slash{E}_{T} [GeV]");
+     hIso_vs_MET[i]->Sumw2();
+     histograms_2D.push_back(hIso_vs_MET[i]);
+     histograms_MC_2D.push_back(hIso_vs_MET[i]);
+////
+     h_Muon_Pt[i] = new TH1F(Form("h_Muon_Pt_S%i",i), "Muon Pt", 40, 0, 200);
+     h_Muon_Pt[i]->SetXTitle("Muon Pt [GeV]");
+     h_Muon_Pt[i]->Sumw2();
+     histograms.push_back(h_Muon_Pt[i]);
+     histograms_MC.push_back(h_Muon_Pt[i]);
+    ///// 
+     h_NVertex[i] = new TH1F(Form("h_NVertex_S%i",i), "Number of Vertex", 40 , 0, 40);
+     h_NVertex[i]->SetXTitle("Number of Vertices");
+     h_NVertex[i]->Sumw2();
+     histograms.push_back(h_NVertex[i]);
+     histograms_MC.push_back(h_NVertex[i]);
+    
+     h_NMuon[i] = new TH1F(Form("h_NMuon_S%i",i), "Number of Muons", 7, 0, 7);
+     h_NMuon[i]->SetXTitle("Number of Muons");
+     h_NMuon[i]->Sumw2();
+     histograms.push_back(h_NMuon[i]);
+     histograms_MC.push_back(h_NMuon[i]);
+     //
+     h_MuonIso[i] = new TH1F(Form("h_MuonIso_S%i",i), "Rel Iso", 40, 0, 0.2);
+     h_MuonIso[i]->SetXTitle("Rel Isolation");
+     h_MuonIso[i]->Sumw2();
+     histograms.push_back(h_MuonIso[i]);
+     histograms_MC.push_back(h_MuonIso[i]);
+ 
+     h_WMuon_MT[i] = new TH1F(Form("h_WMuon_MT_S%i",i), "Transverse mass", 50, 0, 200);
+     h_WMuon_MT[i]->SetXTitle("MT [GeV]");
+     h_WMuon_MT[i]->Sumw2();
+     histograms.push_back(h_WMuon_MT[i]);
+     histograms_MC.push_back(h_WMuon_MT[i]);
+
+     h_WMuon_Phi[i] = new TH1F(Form("h_WMuon_Phi_S%i",i), "DR between muon and MET", 70, 0, 3.5);
+     h_WMuon_Phi[i]->SetXTitle("DR between muon and MET");
+     h_WMuon_Phi[i]->Sumw2();
+     histograms.push_back(h_WMuon_Phi[i]);
+     histograms_MC.push_back(h_WMuon_Phi[i]);
+
+     h_NJet[i] = new TH1F(Form("h_NJet_S%i",i), "Jet Multiplicity", 14, 0, 14);
+     h_NJet[i]->SetXTitle("Jet Multiplicity");
+     h_NJet[i]->Sumw2();
+     histograms.push_back(h_NJet[i]);
+     histograms_MC.push_back(h_NJet[i]);
+
+     h_NBJet[i] = new TH1F(Form("h_NBJet_S%i",i), "b tagged Jet Multiplicity", 5, 0, 5);
+     h_NBJet[i]->SetXTitle("CSVv2M  b-tagged Jet Multiplicity");
+     h_NBJet[i]->Sumw2();
+     histograms.push_back(h_NBJet[i]);
+     histograms_MC.push_back(h_NBJet[i]);
+
+     h_MET[i] = new TH1F(Form("h_MET_S%i",i), "MET", 40, 0, 200);
+     h_MET[i]->SetXTitle("#slash{E}_{T} [GeV]");
+     h_MET[i]->Sumw2();
+     histograms.push_back(h_MET[i]);
+     histograms_MC.push_back(h_MET[i]);
+
+   }
+
+}
+
+Bool_t MyAnalysis::Process(Long64_t entry) {
+   // The return value is currently not used.
+   bool debug = false;
+ 
+   ++TotalEvents;
+   
+   GetEntry(entry);
+
+   if (TotalEvents % 1000000 == 0)
+      cout << "Next event -----> " << TotalEvents << endl;
+   
+   BuildEvent();
+   double Muon_PtCut = 30.;
+   double MuonEtaCut = 30.;
+   double MuonRelIsoCut = 0.12;
+   double ElectronPtCut = 30.;
+   double ElectronEtaCut = 30.;
+   double ElectronRelIsoCut = 0.12;
+   int N_IsoMuon = 0;
+
+   int sel_mu = 0;
+   int imu = 0;
+
+   MyMuon *muon1, *muon2;
+   for (vector<MyMuon>::iterator jt = Muons.begin(); jt != Muons.end(); ++jt) {
+     if( Muon_Iso[imu] < MuonRelIsoCut){
+       N_IsoMuon++;
+       if( N_IsoMuon == 1 ) {
+         muon1 = &(*jt); 
+         sel_mu = imu; 
+       }
+       if( N_IsoMuon == 2 ) muon2 = &(*jt); 
+     }
+     imu++;
+   }
+
+   int N_IsoElectron = 0;
+   
+   for (int i = 0; i < NElectron; ++i) {
+     if( Electron_Iso[i] < ElectronRelIsoCut){
+       N_IsoElectron++;
+     }
+   }
+
+   int N_VetoMuon = 0;
+   
+   for (int i = 0; i < NLooseMuon; ++i) {
+     if( LooseMuon_Iso[i] < 0.25){
+       N_VetoMuon++;
+     }
+   }
+
+   int N_VetoElectron = 0;
+
+   for (int i = 0; i < NLooseElectron; ++i) {
+     if( LooseElectron_Iso[i] < 0.25){
+       N_VetoElectron++;
+     }
+   }
+
+   if( debug ) cout << "filling at step0..." << endl;
+   //////////////////////////////
+   //step 0 
+   if( NMuon > 0 )
+    {
+     hIso_vs_MET[0]->Fill(MET, Muon_Iso[0], EventWeight);
+     h_Muon_Pt[0]->Fill(Muon_Pt[sel_mu], EventWeight);
+     h_MuonIso[0]->Fill(Muon_Iso[0], EventWeight);
+     h_NMuon[0]->Fill(N_IsoMuon, EventWeight);
+     h_NVertex[0]->Fill(NVertex, EventWeight);
+     h_WMuon_MT[0]->Fill( WMuon_MT[sel_mu], EventWeight);
+     h_WMuon_Phi[0]->Fill( WMuon_Phi[sel_mu], EventWeight);
+     h_NJet[0]->Fill(NJet, EventWeight);
+     h_NBJet[0]->Fill(NBJet, EventWeight);
+     h_MET[0]->Fill(MET, EventWeight);
+    }
+
+   if( debug ) cout << "filling at step1..." << endl;
+   if( NMuon == 1 && N_VetoElectron == 0 && Muon_PtCut )
+     { //step 1
+     hIso_vs_MET[1]->Fill(MET, Muon_Iso[sel_mu], EventWeight);
+     h_Muon_Pt[1]->Fill(Muon_Pt[sel_mu], EventWeight);
+     h_MuonIso[1]->Fill(Muon_Iso[sel_mu], EventWeight); 
+     h_NMuon[1]->Fill(N_IsoMuon, EventWeight);
+     h_NVertex[1]->Fill(NVertex, EventWeight);
+     h_WMuon_MT[1]->Fill( WMuon_MT[sel_mu], EventWeight);
+     h_WMuon_Phi[1]->Fill( WMuon_Phi[sel_mu], EventWeight);
+     h_NJet[1]->Fill(NJet, EventWeight);
+     h_NBJet[1]->Fill(NBJet, EventWeight); 
+     h_MET[1]->Fill(MET, EventWeight);
+     if( debug ) cout << "filling at step2..." << endl;
+     if( NJet > 3)
+     { //step 2
+       hIso_vs_MET[2]->Fill(MET, Muon_Iso[sel_mu], EventWeight);
+       h_Muon_Pt[2]->Fill(Muon_Pt[sel_mu], EventWeight);
+       h_MuonIso[2]->Fill(Muon_Iso[sel_mu], EventWeight); 
+       h_NMuon[2]->Fill(N_IsoMuon, EventWeight);
+       h_NVertex[2]->Fill(NVertex, EventWeight);
+       h_WMuon_MT[2]->Fill( WMuon_MT[sel_mu], EventWeight);
+       h_WMuon_Phi[2]->Fill( WMuon_Phi[sel_mu], EventWeight);
+       h_NJet[2]->Fill(NJet, EventWeight);
+       h_NBJet[2]->Fill(NBJet, EventWeight);
+       h_MET[2]->Fill(MET, EventWeight);
+     
+//////steop3 MET
+     if( debug ) cout << "filling at step3..." << endl;
+       if( NBJet > 1 )
+     { //step 3
+         hIso_vs_MET[3]->Fill(MET, Muon_Iso[sel_mu], EventWeight);
+         h_Muon_Pt[3]->Fill(Muon_Pt[sel_mu], EventWeight);
+         h_MuonIso[3]->Fill(Muon_Iso[sel_mu], EventWeight); 
+         h_NMuon[3]->Fill(N_IsoMuon, EventWeight);
+         h_NVertex[3]->Fill(NVertex, EventWeight);
+         h_WMuon_MT[3]->Fill( WMuon_MT[sel_mu], EventWeight);
+         h_WMuon_Phi[3]->Fill( WMuon_Phi[sel_mu], EventWeight);
+         h_NJet[3]->Fill(NJet, EventWeight);
+         h_NBJet[3]->Fill(NBJet, EventWeight);
+         h_MET[3]->Fill(MET, EventWeight);
+      } //step3
+  
+     }//step2
+  } //step1
+   if( debug ) cout << "ending event..." << endl; 
+   return kTRUE;
+}
+
+void MyAnalysis::SlaveTerminate() {
+   // The SlaveTerminate() function is called after all entries or objects
+}
+void MyAnalysis::Terminate() {
+   // The Terminate() function is the last function to be called during
+}
---
+++ HEP763/MyAnalysis_qcd.h
@@ -0,0 +1,329 @@
+#ifndef MyAnalysis_h
+#define MyAnalysis_h
+
+#include <TROOT.h>
+#include <TFile.h>
+#include <TChain.h>
+#include <TSelector.h>
+#include <TH1F.h>
+#include <TH1D.h>
+#include <TH2F.h>
+#include <TH2D.h>
+#include <TLorentzVector.h>
+#include <vector>
+
+#include "MyJet.h"
+#include "MyMuon.h"
+#include "MyElectron.h"
+#include "MyPhoton.h"
+#include <iostream>
+using namespace std;
+// Header file for the classes stored in the TTree if any.
+// Fixed size dimensions of array or collections stored in the TTree if any.
+class MyAnalysis: public TSelector {
+public:
+   TTree *fChain; //!pointer to the analyzed TTree or TChain
+   // Declaration of leaf types
+   Int_t NBJet;
+   Int_t NJet;
+   Float_t Jet_Pt[10]; //[NJet]
+   Float_t Jet_Eta[10]; //[NJet]
+   Float_t Jet_Phi[10]; //[NJet]
+   Float_t Jet_E[10]; //[NJet]
+   Float_t Jet_bDiscriminator[10]; //[NJet]
+   Float_t Jet_ID[10]; //[NJet]
+   Int_t NMuon;
+   Float_t Muon_Pt[5]; //[NMuon]
+   Float_t Muon_Eta[5]; //[NMuon]
+   Float_t Muon_Phi[5]; //[NMuon]
+   Float_t Muon_E[5]; //[NMuon]
+   Int_t Muon_Charge[5]; //[NMuon]
+   Float_t Muon_Iso[5]; //[NMuon]
+   Int_t NLooseMuon;
+   Float_t LooseMuon_Pt[5]; //[NLooseMuon]
+   Float_t LooseMuon_Eta[5]; //[NLooseMuon]
+   Float_t LooseMuon_Phi[5]; //[NLooseMuon]
+   Float_t LooseMuon_E[5]; //[NLooseMuon]
+   Int_t LooseMuon_Charge[5]; //[NLooseMuon]
+   Float_t LooseMuon_Iso[5]; //[NLooseMuon]
+   Int_t NElectron;
+   Float_t Electron_Pt[5]; //[NElectron]
+   Float_t Electron_Eta[5]; //[NElectron]
+   Float_t Electron_Phi[5]; //[NElectron]
+   Float_t Electron_E[5]; //[NElectron]
+   Int_t Electron_Charge[5]; //[NElectron]
+   Float_t Electron_Iso[5]; //[NElectron]
+   Int_t NLooseElectron;
+   Float_t LooseElectron_Pt[5]; //[NLooseElectron]
+   Float_t LooseElectron_Eta[5]; //[NLooseElectron]
+   Float_t LooseElectron_Phi[5]; //[NLooseElectron]
+   Float_t LooseElectron_E[5]; //[NLooseElectron]
+   Int_t LooseElectron_Charge[5]; //[NLooseElectron]
+   Float_t LooseElectron_Iso[5]; //[NLooseElectron]
+ 
+   Int_t NPhoton;
+   Float_t Photon_Pt[5]; //[NPhoton]
+   Float_t Photon_Eta[5]; //[NPhoton]
+   Float_t Photon_Phi[5]; //[NPhoton]
+   Float_t Photon_E[5]; //[NPhoton]
+   Float_t Photon_Iso[5]; //[NPhoton]
+   Double_t MET;
+   Double_t MET_Px;
+   Double_t MET_Py;
+
+   Float_t WMuon_MT[5];
+   Float_t WMuon_Phi[5];
+   Float_t WElectron_MT[5];
+   Float_t WElectron_Phi[5];
+
+   Float_t MChadronicBottom_px;
+   Float_t MChadronicBottom_py;
+   Float_t MChadronicBottom_pz;
+   Float_t MCleptonicBottom_px;
+   Float_t MCleptonicBottom_py;
+   Float_t MCleptonicBottom_pz;
+   Float_t MChadronicWDecayQuark_px;
+   Float_t MChadronicWDecayQuark_py;
+   Float_t MChadronicWDecayQuark_pz;
+   Float_t MChadronicWDecayQuarkBar_px;
+   Float_t MChadronicWDecayQuarkBar_py;
+   Float_t MChadronicWDecayQuarkBar_pz;
+   Float_t MClepton_px;
+   Float_t MClepton_py;
+   Float_t MClepton_pz;
+   Int_t MCleptonPDGid;
+   Float_t MCneutrino_px;
+   Float_t MCneutrino_py;
+   Float_t MCneutrino_pz;
+   Int_t NPrimaryVertices;
+   //Bool_t triggerIsoMu24;
+   Int_t NVertex;
+   Int_t DiLeptonic;
+   Int_t SemiLeptonic;
+   Float_t PUWeight;
+   Float_t GenWeight;
+   
+   // List of branches
+   TBranch *b_DiLeptonic;
+   TBranch *b_SemiLeptonic; //! 
+   TBranch *b_NBJet; //!
+   TBranch *b_NJet; //!
+   TBranch *b_Jet_Pt; //!
+   TBranch *b_Jet_Eta; //!
+   TBranch *b_Jet_Phi; //!
+   TBranch *b_Jet_E; //!
+   TBranch *b_Jet_bDiscriminator; //!
+   TBranch *b_Jet_ID; //!
+   TBranch *b_NMuon; //!
+   TBranch *b_Muon_Pt; //!
+   TBranch *b_Muon_Eta; //!
+   TBranch *b_Muon_Phi; //!
+   TBranch *b_Muon_E; //!
+   TBranch *b_Muon_Charge; //!
+   TBranch *b_Muon_Iso; //!
+   TBranch *b_NLooseMuon; //!
+   TBranch *b_LooseMuon_Pt; //!
+   TBranch *b_LooseMuon_Eta; //!
+   TBranch *b_LooseMuon_Phi; //!
+   TBranch *b_LooseMuon_E; //!
+   TBranch *b_LooseMuon_Charge; //!
+   TBranch *b_LooseMuon_Iso; //!
+
+   TBranch *b_NElectron; //!
+   TBranch *b_Electron_Pt; //!
+   TBranch *b_Electron_Eta; //!
+   TBranch *b_Electron_Phi; //!
+   TBranch *b_Electron_E; //!
+   TBranch *b_Electron_Charge; //!
+   TBranch *b_Electron_Iso; //!
+   TBranch *b_NLooseElectron; //!
+   TBranch *b_LooseElectron_Pt; //!
+   TBranch *b_LooseElectron_Eta; //!
+   TBranch *b_LooseElectron_Phi; //!
+   TBranch *b_LooseElectron_E; //!
+   TBranch *b_LooseElectron_Charge; //!
+   TBranch *b_LooseElectron_Iso; //!
+
+
+   TBranch *b_NPhoton; //!
+   TBranch *b_Photon_Pt; //!
+   TBranch *b_Photon_Eta; //!
+   TBranch *b_Photon_Phi; //!
+   TBranch *b_Photon_E; //!
+   TBranch *b_Photon_Iso; //!
+   TBranch *b_MET; //!
+   TBranch *b_MET_Px; //!
+   TBranch *b_MET_Py; //!
+
+   TBranch *b_WMuon_MT;
+   TBranch *b_WMuon_Phi;
+   TBranch *b_WElectron_MT;
+   TBranch *b_WElectron_Phi;
+
+   TBranch *b_MChadronicBottom_px; //!
+   TBranch *b_MChadronicBottom_py; //!
+   TBranch *b_MChadronicBottom_pz; //!
+   TBranch *b_MCleptonicBottom_px; //!
+   TBranch *b_MCleptonicBottom_py; //!
+   TBranch *b_MCleptonicBottom_pz; //!
+   TBranch *b_MChadronicWDecayQuark_px; //!
+   TBranch *b_MChadronicWDecayQuark_py; //!
+   TBranch *b_MChadronicWDecayQuark_pz; //!
+   TBranch *b_MChadronicWDecayQuarkBar_px; //!
+   TBranch *b_MChadronicWDecayQuarkBar_py; //!
+   TBranch *b_MChadronicWDecayQuarkBar_pz; //!
+   TBranch *b_MClepton_px; //!
+   TBranch *b_MClepton_py; //!
+   TBranch *b_MClepton_pz; //!
+   TBranch *b_MCleptonPDGid; //!
+   TBranch *b_MCneutrino_px; //!
+   TBranch *b_MCneutrino_py; //!
+   TBranch *b_MCneutrino_pz; //!
+   TBranch *b_NPrimaryVertices; //!
+   //TBranch *b_triggerIsoMu24; //!
+   TBranch *b_NVertex; //!
+   TBranch *b_PUWeight; //!
+   TBranch *b_GenWeight; //!
+   
+   MyAnalysis(float sf = 1., float wf = 1, float Xsection = 1.0 , float lumi = 1.0, float num = 1.0, TTree * /*tree*/= 0) :
+   fChain(0) {
+      weight_factor = wf;
+      norm_scale = lumi/(num/Xsection);
+      SF_b = sf;
+   }
+
+   virtual ~MyAnalysis() {
+   }
+   virtual Int_t Version() const {
+      return 2;
+   }
+   virtual void Begin(TTree *tree);
+   virtual void SlaveBegin(TTree *tree);
+   virtual void Init(TTree *tree);
+   virtual Bool_t Notify();
+   virtual Bool_t Process(Long64_t entry);
+   virtual Int_t GetEntry(Long64_t entry, Int_t getall = 0) {
+      return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0;
+   }
+   virtual void SetOption(const char *option) {
+      fOption = option;
+   }
+   virtual void SetObject(TObject *obj) {
+      fObject = obj;
+   }
+   virtual void SetInputList(TList *input) {
+      fInput = input;
+   }
+   virtual TList *GetOutputList() const {
+      return fOutput;
+   }
+   virtual void SlaveTerminate();
+   virtual void Terminate();
+   
+   void BuildEvent();
+  
+   float EventWeight; 
+   int TotalEvents;
+   vector<MyJet> Jets;
+   vector<MyMuon> Muons;
+   vector<MyElectron> Electrons;
+   vector<MyPhoton> Photons;
+   
+   TLorentzVector hadB, lepB, hadWq, hadWqb, lepWl, lepWn;
+   TLorentzVector mymet;
+   
+   float weight_factor;
+   float norm_scale;
+   float SF_b;
+ 
+   TH1F *h_Muon_Pt[4];
+   TH1F *h_NMuon[4];
+   TH1F *h_MuonIso[4];
+   TH1F *h_NVertex[4];
+   TH1F *h_WMuon_MT[4];
+   TH1F *h_WMuon_Phi[4];
+   TH1F *h_NJet[4]; 
+   TH1F *h_NBJet[4]; 
+   TH1F *h_MET[4]; 
+   TH2D *hIso_vs_MET[4]; 
+
+   vector<TH1F*> histograms;
+   vector<TH1F*> histograms_MC;
+   vector<TH2D*> histograms_2D;  
+   vector<TH2D*> histograms_MC_2D;  
+};
+
+#endif
+
+#ifdef MyAnalysis_cxx
+void MyAnalysis::Init(TTree *tree)
+{
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fChain->SetMakeClass(1);
+   
+   fChain->SetBranchAddress("DiLeptonic", &DiLeptonic, &b_DiLeptonic);
+   fChain->SetBranchAddress("SemiLeptonic", &SemiLeptonic, &b_SemiLeptonic);
+   fChain->SetBranchAddress("NBJet", &NBJet, &b_NBJet);
+   fChain->SetBranchAddress("NJet", &NJet, &b_NJet);
+   fChain->SetBranchAddress("Jet_Pt", Jet_Pt, &b_Jet_Pt);
+   fChain->SetBranchAddress("Jet_Eta", Jet_Eta, &b_Jet_Eta);
+   fChain->SetBranchAddress("Jet_Phi", Jet_Phi, &b_Jet_Phi);
+   fChain->SetBranchAddress("Jet_E", Jet_E, &b_Jet_E);
+   fChain->SetBranchAddress("Jet_bDiscriminator", Jet_bDiscriminator, &b_Jet_bDiscriminator);
+   //fChain->SetBranchAddress("Jet_ID", Jet_ID, &b_Jet_ID);
+   fChain->SetBranchAddress("NMuon", &NMuon, &b_NMuon);
+   fChain->SetBranchAddress("Muon_Pt", Muon_Pt, &b_Muon_Pt);
+   fChain->SetBranchAddress("Muon_Eta", Muon_Eta, &b_Muon_Eta);
+   fChain->SetBranchAddress("Muon_Phi", Muon_Phi, &b_Muon_Phi);
+   fChain->SetBranchAddress("Muon_E", Muon_E, &b_Muon_E);
+   fChain->SetBranchAddress("Muon_Charge", Muon_Charge, &b_Muon_Charge);
+   fChain->SetBranchAddress("Muon_Iso", Muon_Iso, &b_Muon_Iso);
+   fChain->SetBranchAddress("NLooseMuon", &NLooseMuon, &b_NLooseMuon);
+   fChain->SetBranchAddress("LooseMuon_Pt", LooseMuon_Pt, &b_LooseMuon_Pt);
+   fChain->SetBranchAddress("LooseMuon_Eta", LooseMuon_Eta, &b_LooseMuon_Eta);
+   fChain->SetBranchAddress("LooseMuon_Phi", LooseMuon_Phi, &b_LooseMuon_Phi);
+   fChain->SetBranchAddress("LooseMuon_E", LooseMuon_E, &b_LooseMuon_E);
+   fChain->SetBranchAddress("LooseMuon_Charge", LooseMuon_Charge, &b_LooseMuon_Charge);
+   fChain->SetBranchAddress("LooseMuon_Iso", LooseMuon_Iso, &b_LooseMuon_Iso);
+
+   fChain->SetBranchAddress("NElectron", &NElectron, &b_NElectron);
+   fChain->SetBranchAddress("Electron_Pt", Electron_Pt, &b_Electron_Pt);
+   fChain->SetBranchAddress("Electron_Eta", Electron_Eta, &b_Electron_Eta);
+   fChain->SetBranchAddress("Electron_Phi", Electron_Phi, &b_Electron_Phi);
+   fChain->SetBranchAddress("Electron_E", Electron_E, &b_Electron_E);
+   fChain->SetBranchAddress("Electron_Charge", Electron_Charge, &b_Electron_Charge);
+   fChain->SetBranchAddress("Electron_Iso", Electron_Iso, &b_Electron_Iso);
+   fChain->SetBranchAddress("NLooseElectron", &NLooseElectron, &b_NLooseElectron);
+   fChain->SetBranchAddress("LooseElectron_Pt", LooseElectron_Pt, &b_LooseElectron_Pt);
+   fChain->SetBranchAddress("LooseElectron_Eta", LooseElectron_Eta, &b_LooseElectron_Eta);
+   fChain->SetBranchAddress("LooseElectron_Phi", LooseElectron_Phi, &b_LooseElectron_Phi);
+   fChain->SetBranchAddress("LooseElectron_E", LooseElectron_E, &b_LooseElectron_E);
+   fChain->SetBranchAddress("LooseElectron_Charge", LooseElectron_Charge, &b_LooseElectron_Charge);
+   fChain->SetBranchAddress("LooseElectron_Iso", LooseElectron_Iso, &b_LooseElectron_Iso);
+
+   fChain->SetBranchAddress("MET", &MET, &b_MET);
+   fChain->SetBranchAddress("MET_Px", &MET_Px, &b_MET_Px);
+   fChain->SetBranchAddress("MET_Py", &MET_Py, &b_MET_Py);
+
+   fChain->SetBranchAddress("WMuon_MT", &WMuon_MT, &b_WMuon_MT);
+   fChain->SetBranchAddress("WMuon_Phi", &WMuon_Phi, &b_WMuon_Phi);
+   fChain->SetBranchAddress("WElectron_MT", &WElectron_MT, &b_WElectron_MT);
+   fChain->SetBranchAddress("WElectron_Phi", &WElectron_Phi, &b_WElectron_Phi);
+ //  fChain->SetBranchAddress("triggerIsoMu24", &triggerIsoMu24, &b_triggerIsoMu24);
+   fChain->SetBranchAddress("NVertex", &NVertex, &b_NVertex);
+   fChain->SetBranchAddress("PUWeight", &PUWeight, &b_PUWeight);
+   fChain->SetBranchAddress("GenWeight", &GenWeight, &b_GenWeight);
+   
+   TotalEvents = 0;
+   EventWeight = 1.0;
+
+}
+
+Bool_t MyAnalysis::Notify()
+{
+   return kTRUE;
+}
+
+#endif // #ifdef MyAnalysis_cxx
